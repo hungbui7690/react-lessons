03b41bff6a30ba3a67bf6e1ebac30514f8247eb1
Hung Bui <hungbui7690@gmail.com>
2023-03-03 03:17:57 -0700

    Array.map()

--- 02-ES6-Refresher/react-app/src/index.js
+++ 02-ES6-Refresher/react-app/src/index.js
@@ -1,27 +1,8 @@
 /*
-  ARROW FUNCTION & "THIS"
-  > arrow function does not have its own "this" keyword
+  Array.map()
 */
 
-// Solution 1
-const person = {
-  talk() {
-    const self = this // (1)
-    setTimeout(function () {
-      console.log('self:', self) // (2)
-    })
-  },
-}
+const colors = ['red', 'green', 'blue']
+const newColors = colors.map((color) => `<li>${color}</li>`)
 
-// Solution 2
-const personX = {
-  walk() {
-    // (***) use arrow function > "this" keyword of arrow function will inherits the context of surrounding scope
-    setTimeout(() => {
-      console.log('this:', this)
-    })
-  },
-}
-
-person.talk() // person
-personX.walk() // personX
+console.log(newColors)
