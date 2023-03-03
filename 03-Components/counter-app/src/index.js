9ff0c5cd0084a8a233ce176764ba407653db7646
Hung Bui <hungbui7690@gmail.com>
2023-03-03 03:36:17 -0700

    Specifying Children

--- 03-Components/counter-app/src/index.js
+++ 03-Components/counter-app/src/index.js
@@ -1,8 +1,6 @@
 /*
-  YOUR FIRST REACT COMPONENT
-  
-  (1) create /components/counter.jsx
-      > jsx : better compilation than js 
+  SPECIFYING CHILDREN
+  (1) counter.jsx
 
 */
 
@@ -11,13 +9,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
-
-// (3a)
 import Counter from './components/counter'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-// (3b)
 root.render(
   <React.StrictMode>
     <Counter />
