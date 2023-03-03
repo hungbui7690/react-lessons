96a9fc0589c2a427ea776396af71818b595369c1
Hung Bui <hungbui7690@gmail.com>
2023-03-03 03:20:28 -0700

    Classes

--- 02-ES6-Refresher/react-app/src/index.js
+++ 02-ES6-Refresher/react-app/src/index.js
@@ -1,14 +1,33 @@
 /*
-  SPREAD OPERATOR
+  CLASSES
 */
 
-const first = { name: 'Joe' }
-const second = { job: 'Developer' }
+// if we want to create another object with the same properties & methods > need to copy > DRY
+const person = {
+  name: 'Mosh',
+  walk() {
+    console.log('Walk')
+  },
+}
 
-// combined
-const combined = { ...first, ...second, location: 'City X' }
-console.log('combined :>> ', combined)
+//////////////////////////////////////
 
-// cloned
-const cloned = { ...first }
-console.log('cloned :>> ', cloned)
+// blue print
+class Person {
+  constructor(name) {
+    this.name = name
+
+    // instance method
+    this.sleep = function () {
+      console.log('Sleep')
+    }
+  }
+
+  // prototype method
+  talk() {
+    console.log('Talk')
+  }
+}
+
+const personX = new Person('Joe')
+console.log(personX)
